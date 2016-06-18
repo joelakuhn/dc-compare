1fcf788ddb35cc19c4df503a65f958bdff10e096
Joel Kuhn <joelakuhn@gmail.com>
2016-06-17 21:24:21 -0400

    Added verse leveling which keeps verses which are the same next to each other.

--- js/dc-compare.js
+++ js/dc-compare.js
@@ -49,7 +49,6 @@ function cleanup(diff, which) {
 
 function format_text(text) {
   return text
-  .replace(/\n/g, "<br>")
   .replace(/(verily)/ig, '$1 &#x1F37A;')
   .replace(/(behold)/ig, '$1 &#x1F37A;')
   .replace(/(came to pass)/ig, '$1 &#x1F37A;')
@@ -58,18 +57,31 @@ function format_text(text) {
 }
 
 function show_verse(out_id, diff) {
-  var output = '';
+  var output = '<div class="divider"></div>';
   for (var i=0; i<diff.length; i++) {
     var d = diff[i];
+    var pieces = d.value.split("\n");
 
-    if (d.added) {
-      output += '<span class="added">' + format_text(d.value) + '</span>';
-    }
-    else if (d.removed) {
-      output += '<span class="removed">' + format_text(d.value) + '</span>';
-    }
-    else {
-      output += '<span class="same index-' + d.index + '" data-index="' + d.index + '">' + format_text(d.value) + '</span>';
+    var is_first_piece = true;
+    for (var p=0; p<pieces.length; p++) {
+
+      if (p > 0 && !is_first_piece) {
+        output += '<div class="divider"></div>';
+      }
+      is_first_piece = false;
+
+      if (pieces[p].length == 0) continue;
+
+      if (d.added) {
+        output += '<span class="added">' + format_text(pieces[p]) + '</span>';
+      }
+      else if (d.removed) {
+        output += '<span class="removed">' + format_text(pieces[p]) + '</span>';
+      }
+      else {
+        output += '<span class="same index-' + d.index + '_' + p + '" data-index="' + d.index + '_' + p + '">' + format_text(pieces[p]) + '</span>';
+      }
+
     }
   }
   document.getElementById(out_id).innerHTML = output
@@ -88,34 +100,59 @@ function compare_texts(t1, t2, o1, o2) {
 
 function level_intros() {
   $('.intro').height('auto');
-  // setTimeout(function() {
-    var a_height = $('#intro_a').height();
-    var b_height = $('#intro_b').height();
-    var max_height = Math.max(a_height, b_height);
-    $('#intro_a').height(max_height);
-    $('#intro_b').height(max_height);
-  // }, 0);
+  var a_height = $('#intro_a').height();
+  var b_height = $('#intro_b').height();
+  var max_height = Math.max(a_height, b_height);
+  $('#intro_a').height(max_height);
+  $('#intro_b').height(max_height);
+}
+
+function level_paragraphs() {
+  var common_dividers = [];
+  $('#output_a > .same').each(function(_, lh) {
+    lh = $(lh);
+    var rh = $('#output_b > [data-index="' + lh.attr('data-index') + '"]');
+    var lhd = lh.prevAll('.divider:first');
+    var rhd = rh.prevAll('.divider:first');
+    if (!lhd.hasClass('adjusted') && !rhd.hasClass('adjusted')) {
+      common_dividers.push({ lhd: lhd, rhd: rhd });
+      lhd.addClass('adjusted');
+      rhd.addClass('adjusted');
+    }
+  });
+  common_dividers.forEach(function(divs) {
+    var lhd_offset = divs.lhd.offset()
+    var rhd_offset = divs.rhd.offset()
+    if (lhd_offset && rhd_offset) {
+      if (lhd_offset.top > rhd_offset.top) {
+        divs.rhd.css('padding-bottom', lhd_offset.top - rhd_offset.top);
+      }
+      else {
+        divs.lhd.css('padding-bottom', rhd_offset.top - lhd_offset.top);
+      }
+    }
+  })
 }
 
 function compare_verses() {
   $('.cover').show();
-  try {
+  // try {
     var a = window['dc' + version_a]
     var b = window['dc' + version_b]
     if (!a.chapters[chapter] || !b.chapters[chapter]) {
       if (!a.chapters[chapter]) {
         $('#output_a').text("The " + version_a + " edition does not contain this passage.");
-        $('#output_b').html(b.chapters[chapter].verses.join("<br><br>"));
+        $('#output_b').html(b.chapters[chapter].verses.join("\n"));
       }
       if (!b.chapters[chapter]) {
-        $('#output_a').html(a.chapters[chapter].verses.join("<br><br>"));
+        $('#output_a').html(a.chapters[chapter].verses.join("\n"));
         $('#output_b').text("The " + version_b + " edition does not contain this passage.");
       }
     }
     else {
       compare_texts (
-        a.chapters[chapter].verses.join("\n\n"),
-        b.chapters[chapter].verses.join("\n\n"),
+        a.chapters[chapter].verses.join("\n"),
+        b.chapters[chapter].verses.join("\n"),
         'output_a',
         'output_b' );
       compare_texts (
@@ -124,10 +161,14 @@ function compare_verses() {
         'intro_a',
         'intro_b' );
       level_intros();
+      level_paragraphs();
       attach_events();
       ignore_punctuation();
+      ignore_numbering();
     }
-  } catch (e) { }
+  // } catch (e) {
+  //   console.log(e);
+  // }
   $('.cover').hide();
 }
 
@@ -253,6 +294,20 @@ function ignore_punctuation() {
 }
 
 $('#ignore-punctuation').change(ignore_punctuation);
+
+function ignore_numbering() {
+  // var ignore_numbering = $('#ignore-numbering').prop('checked');
+  // if (ignore_numbering) {
+  //   var numbers = $('.added,.removed').filter(function() {
+  //     return $(this).text().match(/^\d+\s*/);
+  //   }).each(function(element) {
+  //     $(element).html($(element).text().replace(/(^\d+\s*)(.*)/, "$1" + '<span style="background-color:' + $(element).css('background-color') + '">$2</span>'));
+  //   })
+  // }
+  // else {
+  //   $('.added,.removed').css('background', '');
+  // }
+}
 
 /*
 WINDOW ADJUSTMENTS
