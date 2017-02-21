342a9d3400ef064f80c8c24651e69695379a8905
Joel Kuhn <joelakuhn@gmail.com>
2017-02-20 21:41:05 -0500

    Added no results message

--- js/search.js
+++ js/search.js
@@ -39,33 +39,37 @@ function search(term) {
 }
 
 function show_search_results(data) {
-  var contents = $('<div class="search-results"></div>');
   var hits = data.hits.hits;
-  var results = [];
-  for (var i=0; i<hits.length; i++) {
-    var highlight = hits[i].highlight.text.join("<br><br>");
-    var verse = hits[i]._source.verse.match(/^\d+$/) ? (+hits[i]._source.verse + 1) : 'intro'
-    var title = '<h2>' + hits[i]._source.chapter
-      + ':' + verse
-      + ' (' + hits[i]._source.year + ')</h2>';
-    var result_text = '<p class="search-result">' + highlight + '</p>';
-    var links = '<p class="search-links">'
-      + '<a class="close-box update-revelation" href="#L'
-        + 'Y' + hits[i]._source.year
-        + 'C' + hits[i]._source.chapter
-        + 'V' + verse
-        + '">Open on Left</a>'
-      + '&nbsp;&nbsp;'
-      + '<a class="close-box update-revelation" href="#R'
-        + 'Y' + hits[i]._source.year
-        + 'C' + hits[i]._source.chapter
-        + 'V' + verse
-        + '">Open on Right</a>'
-      + '</p>';
-    var result = title + result_text + links;
-    results.push(result);
+  if (hits.length > 0) {
+    var results = [];
+    for (var i=0; i<hits.length; i++) {
+      var highlight = hits[i].highlight.text.join("<br><br>");
+      var verse = hits[i]._source.verse.match(/^\d+$/) ? (+hits[i]._source.verse + 1) : 'intro'
+      var title = '<h2>' + hits[i]._source.chapter
+        + ':' + verse
+        + ' (' + hits[i]._source.year + ')</h2>';
+      var result_text = '<p class="search-result">' + highlight + '</p>';
+      var links = '<p class="search-links">'
+        + '<a class="close-box update-revelation" href="#L'
+          + 'Y' + hits[i]._source.year
+          + 'C' + hits[i]._source.chapter
+          + 'V' + verse
+          + '">Open on Left</a>'
+        + '&nbsp;&nbsp;'
+        + '<a class="close-box update-revelation" href="#R'
+          + 'Y' + hits[i]._source.year
+          + 'C' + hits[i]._source.chapter
+          + 'V' + verse
+          + '">Open on Right</a>'
+        + '</p>';
+      var result = title + result_text + links;
+      results.push(result);
+    }
+    var search_content = '<div class="search-results">' + results.join('') + '</div>';
   }
-  var search_content = '<div class="search-results">' + results.join('') + '</div>';
+  else {
+    search_content = '<div class="search-results">There were no results.</div>';
+  }
   box(search_content);
 }
 
